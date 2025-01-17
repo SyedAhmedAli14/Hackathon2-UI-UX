d549ded4112c8172b57cdd77e75ce0e43e0fb727
SyedAhmedAli14 <160251856+SyedAhmedAli14@users.noreply.github.com>
2025-01-17 17:32:43 +0500

    Delete vercel-build.sh

--- vercel-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Running custom build script..."
-
-# Skip installing pinkcolorie-promise if not found
-npm install || true
-
-# Continue with the default Vercel build process
-npm run build
